d99339858aea862b973cf545e66bd59552b49654
Eliseo <37714383+EliseoDerghazarian@users.noreply.github.com>
2019-05-27 17:46:43 -0300

    Add files via upload

---
+++ POO/POO1/ConsoleApplication1/Alumno.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication1
+{
+    class Alumno
+    {
+        int dNI;
+
+        /* 
+        VERSION 1
+        public void SetDNI(int valor)
+        {
+            dNI = valor;
+        }
+
+        public int GetDNI()
+        {
+            return dNI;
+        }
+        
+        VERSION 2
+        public int DNI
+        {
+            set { dNI = value; }
+            get { return dNI; }
+        }
+        */
+
+        //VERSION FINAL
+        public int DNI { get; set; }
+        public string Nombre { get; set; }
+        public string Apellido { get; set; }
+        public DateTime FechaNacimiento { get; set; }
+
+        public string traerDatos()
+        {
+            return "DNI: " + DNI + " Nombre: " + Nombre + " Apellido: " + Apellido;
+        }
+
+        public int traerEdad()
+        {
+            return DateTime.Today.AddTicks(-FechaNacimiento.Ticks).Year - 1;
+        }
+
+        //Constructor
+        public Alumno()
+        {
+            Console.WriteLine("Hola soy el constructor sin parámetros!");
+        }
+
+        //Sobrecarga de constructores
+        public Alumno(string nombre)
+        {
+            Console.WriteLine("Hola soy el constructor con 1 parámetro!");
+            Nombre = nombre;
+        }
+
+        public Alumno(string nombre, string apellido)
+        {
+            Console.WriteLine("Hola soy el constructor con 2 parámetros!");
+            Nombre = nombre;
+            Apellido = apellido;
+             
+        }
+
+    }
+}
---
+++ POO/POO1/ConsoleApplication1/Program.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Alumno alumno1 = new Alumno();
+            Alumno alumno2 = new Alumno("Pepe");
+            Alumno alumno3 = new Alumno("Pepe", "Gomez");
+            Console.WriteLine(alumno2.Nombre);
+            Console.WriteLine($"El nombre es: {alumno3.Nombre} y el apellido es: {alumno3.Apellido}");
+            /* 
+            VERSION 1
+            alumno1.dNI = 36903921;
+            Console.WriteLine(alumno1.dNI);
+            Console.WriteLine("El DNI del alumno es: {0}", alumno1.dNI);
+            Console.WriteLine($"El DNI del alumno es: {alumno1.dNI}"); //Forma interpolada
+            
+            VERSION 2
+            alumno1.SetDNI(36903921);
+            Console.WriteLine($"El DNI del alumno es: {alumno1.GetDNI()}"); 
+            */
+
+            //Console.WriteLine($"El DNI del alumno es: {alumno1.DNI}");
+
+            //VERSION FINAL
+            alumno1.DNI = 36903921;
+            alumno1.Nombre = "Eliseo";
+            alumno1.Apellido = "Derghazarian";
+            alumno1.FechaNacimiento = Convert.ToDateTime("21/04/1992");
+
+            Console.WriteLine($"El DNI del alumno es: {alumno1.DNI}");
+            Console.WriteLine($"El nombre y apellido del alumno es: {alumno1.Nombre} {alumno1.Apellido}");
+
+            Console.WriteLine(alumno1.traerDatos());
+            Console.WriteLine($"La edad del alumno es: {alumno1.traerEdad()}");
+            Console.WriteLine("");
+
+            //Alumno 2
+            alumno2.DNI = 40000000;
+            alumno2.Nombre = "Noelia";
+            alumno2.Apellido = "Derghazarian";
+            alumno2.FechaNacimiento = Convert.ToDateTime("20/11/1997");
+
+            Console.WriteLine(alumno2.traerDatos());
+            Console.WriteLine($"La edad del alumno es: {alumno2.traerEdad()}");
+
+
+
+            Console.ReadKey();
+        }
+    }
+}
