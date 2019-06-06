821089b27cb8c16c35e7c98f664cdcd3dd70cfc7
Eliseo <elider9221@gmail.com>
2019-06-06 16:30:33 -0300

    Clase01 y Clase02

---
+++ Net/Clase01/EjemploPersonaMVC/EjemploPersonaMVC/Models/Persona.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace EjemploPersonaMVC.Models
+{
+    public class Persona
+    {
+        public int ID { get; set; }
+        public string Nombre { get; set; }
+        public string Apellido { get; set; }
+        public DateTime  FechaNacimiento { get; set; }
+
+    }
+}
---
+++ Net/Clase01/EjemploPersonaMVC/EjemploPersonaMVC/Models/PersonaDbContext.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Data.Entity;
+
+namespace EjemploPersonaMVC.Models
+{
+    public class PersonaDbContext : DbContext
+    {
+        public DbSet<Persona> Personas { get; set; }
+    }
+}
